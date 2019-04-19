bdb8088ecae37e875b9594755564ea4c48195875
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-04-19 14:57:03 +0300

    lesson 1 (3)

--- A2v10.Site/App_apps/Lesson1/sql/lesson1.sql
+++ A2v10.Site/App_apps/Lesson1/sql/lesson1.sql
@@ -45,4 +45,72 @@ begin
 	where Id=@Id;
 end
 go
+------------------------------------------------
+if exists (select * from INFORMATION_SCHEMA.ROUTINES where ROUTINE_SCHEMA=N'a2tutorial' and ROUTINE_NAME=N'Agent.Metadata')
+	drop procedure a2tutorial.[Agent.Metadata]
+go
+------------------------------------------------
+if exists (select * from INFORMATION_SCHEMA.ROUTINES where ROUTINE_SCHEMA=N'a2tutorial' and ROUTINE_NAME=N'Agent.Update')
+	drop procedure a2tutorial.[Agent.Update]
+go
+------------------------------------------------
+if exists(select * from INFORMATION_SCHEMA.DOMAINS where DOMAIN_SCHEMA=N'a2tutorial' and DOMAIN_NAME=N'Agent.TableType' and DATA_TYPE=N'table type')
+	drop type a2tutorial.[Agent.TableType];
+go
+------------------------------------------------
+create type a2tutorial.[Agent.TableType]
+as table(
+	Id bigint null,
+	Code nvarchar(32),
+	[Name] nvarchar(255),
+	[Memo] nvarchar(255)
+)
+go
+------------------------------------------------
+create or alter procedure a2tutorial.[Agent.Metadata]
+as
+begin
+	set nocount on;
+	declare @Agent a2tutorial.[Agent.TableType];
+	select [Agent!Agent!Metadata]=null, * from @Agent;
+end
+go
+------------------------------------------------
+create procedure a2tutorial.[Agent.Update]
+	@UserId bigint,
+	@Agent a2tutorial.[Agent.TableType] readonly,
+	@RetId bigint = null output
+as
+begin
+	set nocount on;
+	set transaction isolation level read committed;
+	set xact_abort on;
+
+	declare @output table(op sysname, id bigint);
+
+
+	merge a2tutorial.Agents as target
+	using @Agent as source
+	on (target.Id = source.Id)
+	when matched then
+		update set 
+			target.[Name] = source.[Name],
+			target.[Code] = source.[Code],
+			target.[Memo] = source.Memo,
+			target.[DateModified] = getdate(),
+			target.[UserModified] = @UserId
+	when not matched by target then 
+		insert ([Name], [Code], Memo, UserCreated, UserModified)
+		values ([Name], [Code], Memo, @UserId, @UserId)
+
+	output 
+		$action op,
+		inserted.Id id
+	into @output(op, id);
+
+	select top(1) @RetId = id from @output;
+
+	exec a2tutorial.[Agent.Load] @UserId, @RetId;
+end
+go
 
