523c76bddb9ad5c1f16670338a55898a6d4b9f92
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-04-25 17:06:22 +0300

    lesson 4

---
+++ A2v10.Site/App_apps/Lesson4/catalog/agent/edit.template.js
@@ -0,0 +1,39 @@
+﻿
+
+const template = {
+	validators: {
+		"Agent.Name":
+			[{ valid:isNameValid, msg:'empty field. @[Reload] needed!' }]
+	},
+	events: {
+		"Model.load": modelLoad
+	},
+	commands: {
+		getPos
+	}
+};
+
+module.exports = template;
+
+
+function isNameValid(agent) {
+	console.dir(agent);
+	return agent.Name.length > 3;
+}
+
+function modelLoad(root) {
+	if (root.Params.Text) {
+		root.Agent.Name = root.Params.Text;
+		root.$defer(() => {
+			root.$setDirty(true);
+		});
+	}
+}
+
+function getPos() {
+	console.dir(navigator.geolocation);
+	navigator.geolocation.getCurrentPosition(function (pos) {
+		console.dir(pos);
+	});
+	alert('got!')
+}
---
+++ A2v10.Site/App_apps/Lesson4/catalog/entity/edit.template.js
@@ -0,0 +1,13 @@
+﻿const template = {
+	validators: {
+		"Entity.Name":
+			[{ valid:isNameValid, msg:'Слишком пусто' }]
+	}
+};
+
+module.exports = template;
+
+function isNameValid(entity) {
+	console.dir(entity);
+	return entity.Name.length > 5;
+}
